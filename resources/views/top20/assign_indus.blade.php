800bee8278dffa436bb831aed9b0fd42e3e131bf
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-01-14 15:29:24 +0800

    Add files via upload

---
+++ resources/views/top20/assign_indus.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.supervisornav')
+@section('content')
+
+<section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Assign Industrial</h5>
+              
+              <table class="table table-hover">
+                       
+              <tr>
+      <div class="card-body col-lg-6" style="margin-left:auto;margin-right:auto;">
+    
+      <form class="row g-3" action="{{ url('/main/' .$result->studentID) }}" method="post">
+          {!! csrf_field() !!}
+            @method("PATCH")
+                <div class="col-12">
+                  <label class="form-label">Student Name:</label>
+                  <input type="text" name="studentName" id="studentName" value="{{$result->studentName}}" class="form-control" disabled><br/>
+                </div>
+
+                <div class="col-12">
+                  <label class="form-label">Matric ID:</label>
+                  <input type="text" name="studentID" id="studentID" value="{{$result->studentID}}" class="form-control" disabled><br/>
+                </div>
+
+                <div class="col-12">
+                  <label class="form-label">Supervisor:</label>
+                  <input type="text" name="stdsupervisor" id="stdsupervisor" value="{{$result->stdsupervisor}}" class="form-control" disabled><br/>
+                </div>
+
+                <div class="col-12">
+                  <label class="form-label">PSM Title:</label>
+                  <input type="text" name="stdpsmtitle" id="stdpsmtitle" value="{{$result->stdpsmtitle}}" class="form-control" disabled><br/>
+                </div>
+
+                <div class="col-12">
+                  <label class="form-label">PSM 1 Marks:</label>
+                  <input type="text" name="PSM1_MARKS" id="PSM1_MARKS" value="{{$result->PSM1_MARKS}}" class="form-control" disabled><br/>
+                </div>
+                <div class="col-12">
+                  <label class="form-label">PSM 2 Marks:</label>
+                  <input type="text" name="PSM2_MARKS" id="PSM2_MARKS"  value="{{$result->PSM2_MARKS}}" class="form-control" disabled><br/>
+                </div>
+
+                <div class="col-12">
+                  <label class="form-label">Assign Industry:</label>
+                  
+                  <div class="col-sm-10">
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="Software" checked>
+                      <label class="form-check-label">
+                      Software
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="Hardware">
+                      <label class="form-check-label">
+                      Hardware
+                      </label>
+                    </div>
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="Graphic">
+                      <label class="form-check-label">
+                      Graphic
+                      </label>
+                    </div>
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="AI">
+                      <label class="form-check-label">
+                      Artificial Intelligence
+                      </label>
+                    </div>
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="Cybersecurity">
+                      <label class="form-check-label">
+                      Cyber Security
+                      </label>
+                    </div>
+
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="industry_status" id="industry_status" value="Others">
+                      <label class="form-check-label">
+                      Others
+                      </label>
+                    </div>
+
+                  </div>
+                </fieldset>
+                    <br/>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>&nbsp&nbsp&nbsp&nbsp
+                  <button type="reset" class="btn btn-secondary">Reset</button>
+                </div>
+              </form>
+       
+    </div>
+    </tr>
+              </table>
+              </center>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      
+    </section> 
+
+@endsection
+
---
+++ resources/views/top20/resultMain.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.supervisornav')
+@section('content')
+
+@if(\Session::has('success'))
+
+  <div class="alert alert-success alert-dismissible fade show" role="alert" style="height:fit-content;">{{\Session::get('success')}}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+        <hr>
+            @endif
+
+  <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">FYP Carnival Marks</h5>
+              
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Matric ID</th>
+                    <th scope="col">Supervisor</th>
+                    <th scope="col">FYP Title</th>
+                    <th scope="col">PSM 1 Mark</th>
+                    <th scope="col">PSM 2 Mark</th>
+                    <th scope="col">Industry</th>
+                    <th scope="col">More Action</th>
+                  </tr>
+                </thead>
+                      <tbody>
+                          @foreach($result as $data)  
+                            <tr>
+                              <th scope="row">{{ $loop->iteration }}</th>
+                              <td>{{$data->studentName}}</td>
+                              <td>{{$data->studentID}}</td>
+                              <td>{{$data->stdsupervisor}}</td>
+                              <td>{{$data->stdpsmtitle}}</td>
+                              <td>{{$data->PSM1_MARKS}}</td>
+                              <td>{{$data->PSM2_MARKS}}</td>
+                              <td>{{$data->industry_status}}</td>
+                              
+                              <td>&nbsp&nbsp&nbsp<a href="{{ url('/main/' . $data->studentID. '/edit')}}" title="Edit"><button class="btn btn-outline-info" style="border-radius: 13px;"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Assign</button></a></td>
+                            </tr>
+                          @endforeach 
+                          </tbody>
+              </table>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <div class="text-center">
+          <a href="{{ url('/main/create')}}" class="btn btn-primary"><i class="fa fa-filter" aria-hidden="true"></i> Generate Top 20</a>&nbsp&nbsp&nbsp
+          <button type="button" onclick="window.print()" class="btn btn-success"><i class="fa fa-cloud-download" aria-hidden="true"></i> Print Result</button>
+      </div>
+      
+    </section>            
+
+ 
+@endsection

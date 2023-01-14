7a738ccaf3297ba4d58f801bb00693438df13b54
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-01-14 15:25:33 +0800

    Add files via upload

---
+++ app/Http/Controllers/top20Controller.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Student;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class top20Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //display all student result
+    public function index()
+    {
+        $result = DB::table('students')
+                            ->join('psm2result', 'psm2result.studentID', '=', 'students.studentID')
+                            ->join('psm1result', 'psm1result.studentID', '=', 'students.studentID')
+                            ->select('students.studentName','students.studentID','students.stdsupervisor', 'students.stdpsmtitle','students.industry_status','psm2result.totalMark as PSM2_MARKS','psm1result.totalMark as PSM1_MARKS')
+                            ->orderby('psm1result.totalMark', 'desc')
+                            ->get();
+        
+        return view('top20.resultMain')->with('result', $result);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //generate top 20
+    public function create()
+    {
+        $result = DB::table('students')
+        ->join('psm2result', 'psm2result.studentID', '=', 'students.studentID')
+        ->join('psm1result', 'psm1result.studentID', '=', 'students.studentID')
+        ->select('students.studentName','students.studentID','students.stdsupervisor', 'students.stdpsmtitle','students.industry_status','psm2result.totalMark as PSM2_MARKS','psm1result.totalMark as PSM1_MARKS')
+        ->orderby('psm2result.totalMark', 'desc')
+        ->limit(20)
+        ->get();
+
+        return view('top20.resultMain',compact('result'))->with('success', 'Top 20 generate successfully!');
+        
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($studentID)
+    {
+        $result = DB::table('students')
+                            ->join('psm2result', 'psm2result.studentID', '=', 'students.studentID')
+                            ->join('psm1result', 'psm1result.studentID', '=', 'students.studentID')
+                            ->select('students.studentName','students.studentID','students.stdsupervisor', 'students.stdpsmtitle','students.industry_status','psm2result.totalMark as PSM2_MARKS','psm1result.totalMark as PSM1_MARKS')
+                            ->where('students.studentID',$studentID)
+                            ->first();
+                            return view('top20.assign_indus')->with('result', $result);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $studentID)
+    {
+        $result = Student::find($studentID);
+        $result->industry_status = $request->input('industry_status');
+        $result->save();
+        return redirect('/main')->with('success', 'Student industry updated successfully!');
+    }
+
+        /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function show($id)
+    // {
+    //     //
+    // }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
+}
